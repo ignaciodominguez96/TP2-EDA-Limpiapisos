f1d3311234535bc7e87047c654523588b5b9633c
Pablo Smolkin <psmolkin@itba.edu.ar>
2019-03-25 13:23:27 -0300

    inicializacion y destruccion de allegro, cambios y comentarios en el main

--- Limpiapisos/Limpiapisos/allegro.c
+++ Limpiapisos/Limpiapisos/allegro.c
@@ -3,8 +3,63 @@
 #include "Floor.h"
 #include "Robot.h"
 #include "Tile.h"
+#include "allegro.h"
+
+allegroStruct_t* allegro_setup(allegroStruct_t* usrAllegro)
+{
+	usrAllegro->event_queue = NULL;
+	usrAllegro->display = NULL;
+	usrAllegro->timer = NULL;
+
+	if (!al_init())//inicio allegro
+	{
+		printf("Could not initialize Allegro!\n");
+		return NULL;
+	}
 
 
+	usrAllegro->display = al_create_display(960, 800);
+	if (!(usrAllegro->display))//inicio display
+	{
+		printf("Could not initialize Display!.\n");
+		return NULL;
+	}
+
+	usrAllegro->event_queue = al_create_event_queue();
+	if (!(usrAllegro->event_queue))//inicio cola de eventos
+	{
+		printf("Could not initialize event queue!.\n");
+		al_destroy_display(usrAllegro->display);
+		return NULL;
+	}
+
+	usrAllegro->timer = al_create_timer(1 / FPS); //FIJARSE POR QUE NO LO RECONOCE (y si conviene 60, esto parece copiado de otro prog)
+	if (!(usrAllegro->timer))//inicio timer IMPORTANTE FUNCIONA  A 60 FPS
+	{
+		printf("Failed to create timer!\n");
+		al_destroy_event_queue(usrAllegro->event_queue);
+		al_destroy_display(usrAllegro->display);
+		return NULL;
+	}
+
+	al_set_window_title(usrAllegro->display, "Limpiapisos");
+
+	al_register_event_source(usrAllegro->event_queue, al_get_display_event_source(usrAllegro->display));
+	al_register_event_source(usrAllegro->event_queue, al_get_timer_event_source(usrAllegro->timer));
+
+	al_start_timer(usrAllegro->timer);
+
+	return usrAllegro;
+
+}
+
+void allegro_destroy(allegroStruct_t* usrAllegro)
+{
+	al_destroy_display(usrAllegro->display);
+	al_destroy_event_queue(usrAllegro->event_queue);
+	al_destroy_timer(usrAllegro->timer);
+	usrAllegro = NULL;
+}
 
 void draw_floor(ALLEGRO_DISPLAY *display, floor_t * floor)
 {
--- Limpiapisos/Limpiapisos/main.c
+++ Limpiapisos/Limpiapisos/main.c
@@ -17,117 +17,80 @@
 int
 main(int argc, char * argv[])
 {
-	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-  ALLEGRO_DISPLAY *display = NULL;
-  ALLEGRO_TIMER *timer;
-
-  if(!al_init())//inicio allegro
-  {
-      printf("Could not initialize Allegro!\n");
-      return -1;
-  }
-
-
-  display = al_create_display(960, 800);
-  if(!display)//inicio display
-  {
-      printf("Could not initialize Display!.\n");
-      return -1;
-  }
-
-  event_queue = al_create_event_queue();
-  if(!event_queue)//inicio cola de eventos
-  {
-      printf("Could not initialize event queue!.\n");
-      al_destroy_display(display);
-      return -1;
-  }
-
-  timer = al_create_timer(1/FPS);
-  if(!timer)//inicio timer IMPORTANTE FUNCIONA  A 60 FPS
-  {
-      printf("Failed to create timer!\n");
-      al_destroy_event_queue(event_queue);
-      al_destroy_display(display);
-      return -1;
-  }
-
-  al_set_window_title(display, "Limpiapisos");
-
-  al_register_event_source(event_queue, al_get_display_event_source(display));
-  al_register_event_source(event_queue, al_get_timer_event_source(timer));
-
-  al_start_timer(timer);
+	int end = 1;
 
 	randomize(); //genera una nueva seed para rand
 
-	userData_t* myData = createUserData();
+	allegroStruct_t* myAllegro = allegro_setup(myAllegro);
 
-	int end = 1;
-
-	if (myData != NULL)
+	if (myAllegro != NULL)
 	{
-		if (parseCmdline(argc, argv, parseCallBack, myData) == 4 && isDataFull(myData)) //Igualado a la cantidad de opciones requeridas. En modo2 podrian ser 3
+
+		userData_t* myData = createUserData();
+
+		if (myData != NULL)
 		{
-
-			if (getUserData(myData, MODE) == MODE_1)
+			if (parseCmdline(argc, argv, parseCallBack, myData) == 4 && isDataFull(myData)) //Igualado a la cantidad de opciones requeridas. En modo2 podrian ser 3
 			{
-				simulation_t * simulation = create_Simulation(getUserData(myData, ROBOTC), getUserData(myData, HEIGHT),
-					getUserData(myData, WIDTH), MODE_1); //llenar parametros
 
-				if (simulation != NULL)
+				if (getUserData(myData, MODE) == MODE_1)
 				{
-					while (end)
+					simulation_t * simulation = create_Simulation(getUserData(myData, ROBOTC), getUserData(myData, HEIGHT),
+						getUserData(myData, WIDTH), MODE_1); //llenar parametros
+
+					if (simulation != NULL)
 					{
-						simulate_Simulation(simulation);
-						draw_floor(display, simulation->floor);
-						draw_robots(display, simulation);
-						al_clean_file(display, simulation->floor)
-						if(is_clear_Floor(simulation->floor))
+						while (end)	//Esto deberia estar dentro de simulate_simulation si no me equivoco!!
 						{
-								end = 0;
+							simulate_Simulation(simulation);
+							draw_floor(display, simulation->floor);
+							draw_robots(display, simulation);
+							al_clean_file(display, simulation->floor)
+							if(is_clear_Floor(simulation->floor))
+							{
+									end = 0;
+							}
+							usleep (1000);
 						}
-						usleep (1000);
-					}
 
-				//	get_tickcount_Simulation(simulation); //mostrar por pantalla o como se desea el tickcount
-					destroy_Simulation(simulation);
-				}
-			}
-			else if (getUserData(myData, MODE) == MODE_2)
-			{
-				unsigned int robot_count;
-				double histogram[HISTOGRAM_SIZE];
-
-				for (robot_count = 1; CONDICION_DE_STOP; robot_count++) //creo que esta relacionado CONDICION_STOP con el 0.1 entre n y n+1
-				{
-
-					double tickcount_sum = 0.0;
-
-					for (unsigned int i = 0; i < CANT_SIMULATIONS_MODE_2; i++)
-					{
-						simulation_t * simulation = create_Simulation(robot_count, getUserData(myData, HEIGHT), getUserData(myData, WIDTH), MODE_2);
-
-						simulate_Simulation(simulation);
-						tickcount_sum += get_tickcount_Simulation(simulation);
+					//	get_tickcount_Simulation(simulation); //mostrar por pantalla o como se desea el tickcount
 						destroy_Simulation(simulation);
+					}
+				}
+				else if (getUserData(myData, MODE) == MODE_2)
+				{
+					unsigned int robot_count;
+					double histogram[HISTOGRAM_SIZE];
+
+					for (robot_count = 1; CONDICION_DE_STOP; robot_count++) //creo que esta relacionado CONDICION_STOP con el 0.1 entre n y n+1
+					{
+
+						double tickcount_sum = 0.0;
+
+						for (unsigned int i = 0; i < CANT_SIMULATIONS_MODE_2; i++)
+						{
+							simulation_t * simulation = create_Simulation(robot_count, getUserData(myData, HEIGHT), getUserData(myData, WIDTH), MODE_2);
+
+							simulate_Simulation(simulation);
+							tickcount_sum += get_tickcount_Simulation(simulation);
+							destroy_Simulation(simulation);
+
+						}
+
+						histogram[robot_count-1] = tickcount_sum / CANT_SIMULATIONS_MODE_2; //ver como hacer la estructura para ir pusheando histogram
 
 					}
 
-					histogram[robot_count-1] = tickcount_sum / CANT_SIMULATIONS_MODE_2; //ver como hacer la estructura para ir pusheando histogram
-
+					//show(histogram);
 				}
 
-				//show(histogram);
 			}
 
+			destroyUserData(myData);
 		}
 
+		allegro_destroy(myAllegro);
 	}
 
-	al_destroy_display(display);
-  al_destroy_event_queue(event_queue);
-  al_destroy_timer(timer);
-	destroyUserData(myData);
 	return 0;
 }
